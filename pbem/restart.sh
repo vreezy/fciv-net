ade96b64df02f0b1617f0411ed4b55ab9c65a611
Andreas Røsdal <andreas.rosdal@gmail.com>
2022-09-06 20:48:40 +0200

    Fix PBEM

--- pbem/restart.sh
+++ pbem/restart.sh
@@ -3,6 +3,6 @@
 
 kill $(ps -ef | grep python| grep pbem | awk '{print $2}')
 sleep 4
-cd /home/freeciv/freeciv-web/freeciv-web/pbem/ && /usr/local/bin/python3 -u /home/freeciv/freeciv-web/freeciv-web/pbem/pbem.py > /home/freeciv/freeciv-web/freeciv-web/logs/pbem.log 2>&1 &
+cd /home/freeciv/fciv-net/pbem/ && /usr/local/bin/python3 -u /home/freeciv/fciv-net/pbem/pbem.py > /home/freeciv/fciv.net/logs/pbem.log 2>&1 &
 echo "restart of PBEM process completed."
 
