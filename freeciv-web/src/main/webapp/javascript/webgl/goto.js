35a1b5d799a05a7d740c3e9cf9653d70e326407e
Andreas Røsdal <andreas.rosdal@gmail.com>
2022-12-18 22:34:31 +0100

    Adjustment of Goto line.

--- freeciv-web/src/main/webapp/javascript/webgl/goto.js
+++ freeciv-web/src/main/webapp/javascript/webgl/goto.js
@@ -74,7 +74,7 @@ function webgl_render_goto_line(start_tile, goto_packet_dir)
       var gotoline = new THREE.Mesh(gotoLineGeometry, material);
 
       gotoline.translateOnAxis(new THREE.Vector3(1,0,0).normalize(), currpos['x'] - 4);
-      gotoline.translateOnAxis(new THREE.Vector3(0,1,0).normalize(), height + 3);
+      gotoline.translateOnAxis(new THREE.Vector3(0,1,0).normalize(), height + 4);
       gotoline.translateOnAxis(new THREE.Vector3(0,0,1).normalize(), currpos['y'] - 1);
       scene.add(gotoline);
       goto_lines.push(gotoline);
