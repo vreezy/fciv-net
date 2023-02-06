0bfca1f0e2765476213dae0e8c8e3138e943b164
Andreas Rosdal <41650711+andreasrosdal@users.noreply.github.com>
2023-02-06 08:22:10 +0100

    Update maputil.js

--- freeciv-web/src/main/webapp/javascript/webgl/maputil.js
+++ freeciv-web/src/main/webapp/javascript/webgl/maputil.js
@@ -28,8 +28,8 @@ function map_to_scene_coords(x, y)
     var hvec = map_to_hex2(new THREE.Vector2(x, y));
     var gx = hvec.x - 0.1;
     var gy = hvec.y - 0.1;
-    result['x'] = Math.floor(-470 + gx * 26);
-    result['y'] = Math.floor(30 + gy * 26);
+    result['x'] = Math.floor(-470 + gx * 22);
+    result['y'] = Math.floor(30 + gy * 22);
   } else {
     result['x'] = Math.floor(-470 + x * mapview_model_width / map['xsize']);
     result['y'] = Math.floor(30 + y * mapview_model_height / map['ysize']);
